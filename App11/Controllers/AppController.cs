5486a86a575cf26b36a54bd3ef87e86ab842ccd1
Рома Г <roma.gulchak@gmail.com>
2021-08-31 00:31:21 +0300

    HW11

---
+++ App11/Controllers/AppController.cs
@@ -0,0 +1,22 @@
+﻿using App11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App11.Controllers
+{
+    public class AppController : Controller
+    {
+        [Route("validate/user")]
+        public IActionResult ValidateUser([FromBody]User user) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ App11/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App11.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression("[A-Za-z1-9_]{3,32}")]
+        public string UserName { get; set; }
+        [RegularExpression("[^\\W\\s\\d]{1,40}")]
+        public string FirstName { get; set; }
+        [RegularExpression("[^\\W\\s\\d]{1,40}")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
